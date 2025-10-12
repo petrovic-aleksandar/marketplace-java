31dd919947bfdabf552b32811921501e57e13cce
Aleksandar Petović <aleksandarpetrovic@live.ac.me>
2025-10-12 18:14:36 +0200

    register user

--- src/main/java/me/aco/endpoint/AuthEndpoint.java
+++ src/main/java/me/aco/endpoint/AuthEndpoint.java
@@ -5,8 +5,10 @@ import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import me.aco.dto.LoginReq;
 import me.aco.dto.TokenResp;
+import me.aco.dto.UserReq;
 import me.aco.model.User;
 import me.aco.service.AuthService;
 import me.aco.service.UserService;
@@ -30,6 +32,21 @@ public class AuthEndpoint {
 		else if (!authService.checkPassword(req, loadedUser))
 			return null;
 		return new TokenResp(JWTUtil.createToken(loadedUser), authService.createAndSaveRefreshToken(req));
+	}
+	
+	@Path("/register")
+	@POST
+	@Consumes({MediaType.APPLICATION_JSON})
+	public Response register(UserReq req) {
+		if (userService.getByUsername(req.getUsername()) != null)
+			return Response.status(409, "Username already taken!").build();
+		try {
+			userService.saveUser(req.toUser());
+			return Response.ok("User created!").build();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(500, e.getMessage()).build();
+		}
 	}
 
 }
