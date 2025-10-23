3b17b05fa31e3018292630ef0e4be3ebb5aca9a3
Aleksandar Petović <aleksandarpetrovic@live.ac.me>
2025-10-23 02:22:35 +0200

    fix

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -7,9 +7,9 @@ insert into itemtype (id, name, description, imagePath) values (1, 'Cars&Vehicle
 insert into itemtype (id, name, description, imagePath) values (2, 'Home&Kitchen', 'Everything for your home, small home and kitchen electronics.', 'Categories/2.jpg');
 insert into itemtype (id, name, description, imagePath) values (3, 'Electronics', 'All kinds of devices. Gaming equipment, laptops, home appliances etc.', 'Categories/3.jpg');
 insert into itemtype (id, name, description, imagePath) values (4, 'Sports&Outdoors', 'Sports clothing and sport requisits.', 'Categories/4.jpg');
-insert into itemtype (id, name, description, imagePath) values (5, 'Women\'s clothing', 'Everything for women.', 'Categories/5.jpg');
+insert into itemtype (id, name, description, imagePath) values (5, 'Womens clothing', 'Everything for women.', 'Categories/5.jpg');
 insert into itemtype (id, name, description, imagePath) values (6, 'Jewelry&Accesories', 'All kinds of jewelry and decorative items.', 'Categories/6.jpg');
-insert into itemtype (id, name, description, imagePath) values (7, 'Mens\'s clothing', 'Clothing items for men', 'Categories/7.jpg');
+insert into itemtype (id, name, description, imagePath) values (7, 'Mens clothing', 'Clothing items for men', 'Categories/7.jpg');
 insert into itemtype (id, name, description, imagePath) values (8, 'Books&Media', 'Books and stuff.', 'Categories/8.jpg');
 insert into itemtype (id, name, description, imagePath) values (9, 'Services', 'For various intelectual or physical services.', 'Categories/9.jpg');
 insert into itemtype (id, name, description, imagePath) values (10, 'Other', 'Whatever else.', 'Categories/10.jpg');
